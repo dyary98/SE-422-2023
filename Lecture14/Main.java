952e4e1388950e413c7e5622c2c13a5238b7fb19
Dyary Yassin <dyaryyasin165@gmail.com>
2023-05-03 06:12:32 +0300

    Final Version of lec 14

--- Lecture14/Main.java
+++ Lecture14/Main.java
@@ -29,7 +29,10 @@ class Thread1 extends Thread{
         boolean result = q.offer(1); //! if it couldnt added data to the Q, returns false |||| No exception here 
         q.offer(1, 10, TimeUnit.SECONDS); //! needs try and catch and keeps trying for 10 seconds if faild to add value 1 to the Q
         q.put(1); //! will put the thread into sleeping mode if couldnt add the elemenet to Q, it wakes up The THread if Q had room to other eleements
-      
+         
+        //!Exception  add()       remove()    element()
+        //!no Exce    offer()     poll()      peek()
+        //!sleep      put()       take()
         while (1==1) {
             q.add(1); // if we specifi a size for our Q, lets say its 100, our loop will go for 100 times and then we get an exception on 101th iteration
             //if u dont have try and catch and u got the exception, your thread will be terminated
@@ -41,3 +44,30 @@ class Thread1 extends Thread{
 class Department{
 
 }
+
+// !we also have LinkedBlockingDeque
+
+/* 
+ * 
+ !LinkedBlockingDeque is a thread-safe implementation of the Deque interface in Java. It is similar to LinkedBlockingQueue but provides support for both FIFO (First-In-First-Out) and LIFO (Last-In-First-Out) operations. It is implemented as a linked list with both head and tail pointers, allowing elements to be added or removed from both ends of the deque.
+
+Here are some important methods of LinkedBlockingDeque:
+
+addFirst(E e) - Adds the specified element at the beginning of the deque. Throws an exception if the deque is full.
+addLast(E e) - Adds the specified element at the end of the deque. Throws an exception if the deque is full.
+offerFirst(E e) - Adds the specified element at the beginning of the deque. Returns true if the operation is successful, false if the deque is full.
+offerLast(E e) - Adds the specified element at the end of the deque. Returns true if the operation is successful, false if the deque is full.
+removeFirst() - Removes and returns the first element of the deque. Throws an exception if the deque is empty.
+removeLast() - Removes and returns the last element of the deque. Throws an exception if the deque is empty.
+pollFirst() - Removes and returns the first element of the deque. Returns null if the deque is empty.
+pollLast() - Removes and returns the last element of the deque. Returns null if the deque is empty.
+getFirst() - Returns the first element of the deque without removing it. Throws an exception if the deque is empty.
+getLast() - Returns the last element of the deque without removing it. Throws an exception if the deque is empty.
+peekFirst() - Returns the first element of the deque without removing it. Returns null if the deque is empty.
+peekLast() - Returns the last element of the deque without removing it. Returns null if the deque is empty.
+LinkedBlockingDeque also supports blocking operations like takeFirst() and takeLast() which block the calling thread until an element becomes available. It also provides methods like offerFirst(E e, long timeout, TimeUnit unit) and offerLast(E e, long timeout, TimeUnit unit) which try to add an element to the deque, blocking the calling thread for the specified time if the deque is full.
+ */
+
+
+ //! linkedTransferedQueue
+ //! DelayQueue homework
